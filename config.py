344eb361c4f465615ab593a9227e4025fe0ee163
JanuBapu <142379561+JanuBapu@users.noreply.github.com>
2025-08-06 12:15:11 +0530

    Update config.py

--- config.py
+++ config.py
@@ -24,8 +24,8 @@ MONGO_DB = os.getenv("MONGO_DB", "")
 OWNER_ID = list(map(int, os.getenv("OWNER_ID", "5357048091 7003967919").split())) # list seperated via space
 DB_NAME = os.getenv("DB_NAME", "telegram_downloader")
 STRING = os.getenv("STRING", None) # optional
-LOG_GROUP = int(os.getenv("LOG_GROUP", "-1001234456")) # optional with -100
-FORCE_SUB = int(os.getenv("FORCE_SUB", "-10012345567")) # optional with -100
+LOG_GROUP = int(os.getenv("LOG_GROUP", "-1002166446304")) # optional with -100
+FORCE_SUB = int(os.getenv("FORCE_SUB", "-1001981131674")) # optional with -100
 MASTER_KEY = os.getenv("MASTER_KEY", "gK8HzLfT9QpViJcYeB5wRa3DmN7P2xUq") # for session encryption
 IV_KEY = os.getenv("IV_KEY", "s7Yx5CpVmE3F") # for decryption
 YT_COOKIES = os.getenv("YT_COOKIES", YTUB_COOKIES)
@@ -34,6 +34,7 @@ FREEMIUM_LIMIT = int(os.getenv("FREEMIUM_LIMIT", "0"))
 PREMIUM_LIMIT = int(os.getenv("PREMIUM_LIMIT", "15000"))
 JOIN_LINK = os.getenv("JOIN_LINK", "https://t.me/+wSO0pLqS7FxkZGM1") # this link for start command message
 ADMIN_CONTACT = os.getenv("ADMIN_CONTACT", "https://t.me/RaolSaheb")
+
 
 
 
